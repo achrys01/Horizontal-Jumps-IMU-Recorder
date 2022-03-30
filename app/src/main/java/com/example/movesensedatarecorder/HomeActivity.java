ba4b90e4223c8085773de8b26b7420e4ed8bba22
Mariah <sabionimah@gmail.com>
2022-03-30 17:58:19 +0200

    saving to external storage

--- app/src/main/java/com/example/movesensedatarecorder/HomeActivity.java
+++ app/src/main/java/com/example/movesensedatarecorder/HomeActivity.java
@@ -2,9 +2,7 @@ package com.example.movesensedatarecorder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.movesensedatarecorder.model.Subject;
@@ -13,10 +11,10 @@ import com.example.movesensedatarecorder.utils.SavingUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,11 +28,15 @@ public class HomeActivity extends AppCompatActivity {
     private List<Subject> subjSet = new ArrayList<>();
     private String FILE_NAME = "subjects_data";
     private String CSV_FILE_NAME = "subjects_data.csv";
+    private static final int CREATE_FILE = 1;
+    private String content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        content = null;
 
         buttonSubj = findViewById(R.id.button_subj);
         buttonSubj.setOnClickListener(v -> {
@@ -57,24 +59,58 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    private void saveToCSV(String content) {
+    private void saveToCSV() {
         try {
-            // content is separated by semi-colon";
-            File file = new File(getApplicationContext().getFilesDir(), CSV_FILE_NAME);
-            Log.i(TAG, file.getPath());
-            if(file.exists())
-                file.delete();
-            file.createNewFile();
-            //if (!file.exists()) {file.createNewFile();}
-            FileWriter fw = new FileWriter(file, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(content);
-            bw.flush();
-            MsgUtils.showToast(getApplicationContext(), "file saved to internal storage");
-            bw.close();
-
+            //save to internal storage
+            saveToInternalStorage();
+            //save to external shared storage
+            saveToExternalStorage();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void saveToInternalStorage() throws IOException {
+        File file = new File(getApplicationContext().getFilesDir(), CSV_FILE_NAME);
+        Log.i(TAG, file.getPath());
+        if (file.exists())
+            file.delete();
+        file.createNewFile();
+        FileWriter fw = new FileWriter(file, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(content);
+        bw.flush();
+        bw.close();
+    }
+
+    private void saveToExternalStorage() {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/csv");
+        intent.putExtra(Intent.EXTRA_TITLE, CSV_FILE_NAME);
+
+        startActivityForResult(intent, CREATE_FILE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == CREATE_FILE) {
+            OutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = getContentResolver().openOutputStream(data.getData());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                assert fileOutputStream != null;
+                fileOutputStream.write(content.getBytes()); //Write the obtained string to csv
+                fileOutputStream.flush();
+                fileOutputStream.close();
+                MsgUtils.showToast(getApplicationContext(), "file saved!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -102,8 +138,8 @@ public class HomeActivity extends AppCompatActivity {
         }
         try {
             String heading = "name,lastName,email,height,weight,subjID";
-            String content = heading + "\n" + recordAsCsv();
-            saveToCSV(content);
+            content = heading + "\n" + recordAsCsv();
+            saveToCSV();
         } catch (Exception e) {
             e.printStackTrace();
             MsgUtils.showToast(getApplicationContext(), "could not export list");
